0ba50506f3512a0abc114e62cd60cf86cace7528
Htat Win <fungiinfection@hotmail.com>
2018-11-06 23:05:48 +0800

    reset features for the game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,3 @@
-console.log("Up and running!");
-
 let cards = [
 {
 rank: "queen",
@@ -24,12 +22,46 @@ cardImage: "images/king-of-diamonds.png"
 ];
 let cardsInPlay = [];
 
+function shuffle(deck) {
+    var i = deck.length,
+        j = 0,
+        k;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        k = deck[i];
+        deck[i] = cards[j];
+        deck[j] = k;
+    }
+};
+
+function resetWrongMatch(){
+
+    let cardElement = document.getElementsByTagName("img");
+    for (var i = 0; i < cards.length; i++){
+        cardElement[i].setAttribute("src","images/back.png");
+        cardElement[i].setAttribute("data-id",i);
+    };
+    cardsInPlay = [];
+};
+
+function resetFinishedMatch(){
+    resetWrongMatch();
+    shuffle(cards);
+};
+
+let x=0;
 function checkForMatch(){
-    if (cardsInPlay.length === 2){
-        if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
+    if (cardsInPlay.length === 4){
+        document.getElementById("game-text").innerHTML = "You won!";
+        setTimeout(resetFinishedMatch,500);
+    }
+    else if (cardsInPlay.length === 2){
+        if (cardsInPlay[0].rank === cardsInPlay[1].rank) {        
             document.getElementById("game-text").innerHTML = "You found a match!";
         } else {
             document.getElementById("game-text").innerHTML = "Sorry, try again.";
+            setTimeout(resetWrongMatch, 250);
         }
     }    
 };
@@ -49,6 +81,7 @@ function createBoard(){
         cardElement.addEventListener("click",flipCard);
         document.getElementById("game-board").appendChild(cardElement);
     }
-}
+    shuffle(cards);
+};
 
 createBoard ();
