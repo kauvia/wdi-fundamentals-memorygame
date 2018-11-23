6a080f31dd40a7c6379fa487defbdc60d9404b60
kauvia <mkhw1989@gmail.com>
2018-11-24 02:56:01 +0800

    Beautified code again(first time was a failure)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,23 @@
-let cards = [
-{
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
-},
-{
-rank: "queen",
-suit: "diamonds",
-cardImage: "images/queen-of-diamonds.png"
-},
-{
-rank: "king",
-suit: "hearts",
-cardImage: "images/king-of-hearts.png"
-},
-{
-rank: "king",
-suit: "diamonds",
-cardImage: "images/king-of-diamonds.png"
-}
+let cards = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
 ];
 let cardsInPlay = [];
 
@@ -28,60 +27,60 @@ function shuffle(deck) {
         k;
 
     while (i--) {
-        j = Math.floor(Math.random() * (i+1));
+        j = Math.floor(Math.random() * (i + 1));
         k = deck[i];
         deck[i] = cards[j];
         deck[j] = k;
     }
 };
 
-function resetWrongMatch(){
+function resetWrongMatch() {
 
     let cardElement = document.getElementsByTagName("img");
-    for (var i = 0; i < cards.length; i++){
-        cardElement[i].setAttribute("src","images/back.png");
-        cardElement[i].setAttribute("data-id",i);
+    for (var i = 0; i < cards.length; i++) {
+        cardElement[i].setAttribute("src", "images/back.png");
+        cardElement[i].setAttribute("data-id", i);
     };
     cardsInPlay = [];
 };
 
-function resetFinishedMatch(){
+function resetFinishedMatch() {
     resetWrongMatch();
     shuffle(cards);
 };
 
-let x=0;
-function checkForMatch(){
-    if (cardsInPlay.length === 4){
+let x = 0;
+
+function checkForMatch() {
+    if (cardsInPlay.length === 4) {
         document.getElementById("game-text").innerHTML = "You won!";
-        setTimeout(resetFinishedMatch,500);
-    }
-    else if (cardsInPlay.length === 2){
-        if (cardsInPlay[0].rank === cardsInPlay[1].rank) {        
+        setTimeout(resetFinishedMatch, 500);
+    } else if (cardsInPlay.length === 2) {
+        if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
             document.getElementById("game-text").innerHTML = "You found a match!";
         } else {
             document.getElementById("game-text").innerHTML = "Sorry, try again.";
             setTimeout(resetWrongMatch, 250);
         }
-    }    
+    }
 };
 
-function flipCard(){
+function flipCard() {
     let cardId = this.getAttribute("data-id");
     cardsInPlay.push(cards[cardId]);
-    this.setAttribute("src",cards[cardId].cardImage);
+    this.setAttribute("src", cards[cardId].cardImage);
     checkForMatch();
 };
 
-function createBoard(){
-    for (var i = 0; i < cards.length; i++){
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
         let cardElement = document.createElement("img");
-        cardElement.setAttribute("src","images/back.png");
-        cardElement.setAttribute("data-id",i);
-        cardElement.addEventListener("click",flipCard);
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
         document.getElementById("game-board").appendChild(cardElement);
     }
     shuffle(cards);
 };
 
-createBoard ();
+createBoard();
